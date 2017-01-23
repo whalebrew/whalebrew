2ff26310759df938bfb832a1320ab0271ad3012f
Ben Firshman <ben@firshman.co.uk>
2017-01-23 15:13:56 +0100

    Add basic search

---
+++ cmd/search.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(searchCommand)
+}
+
+var searchCommand = &cobra.Command{
+	Use:   "search TERM",
+	Short: "Search for packages on Docker Hub",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cli, err := client.NewEnvClient()
+		if err != nil {
+			return err
+		}
+
+		term := "whalebrew/" + args[0]
+		options := types.ImageSearchOptions{Limit: 100}
+		results, err := cli.ImageSearch(context.Background(), term, options)
+		if err != nil {
+			return err
+		}
+
+		for _, res := range results {
+			fmt.Println(res.Name)
+		}
+
+		return nil
+	},
+}
