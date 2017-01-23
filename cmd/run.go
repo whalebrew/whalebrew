aa00066433431b059c0a40e8260fbaa1f9900373
Ben Firshman <ben@firshman.co.uk>
2017-01-23 16:44:43 +0100

    Use exec to run commands

--- cmd/run.go
+++ cmd/run.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func init() {
@@ -22,27 +23,29 @@ var runCommand = &cobra.Command{
 		if err != nil {
 			return err
 		}
+		dockerPath, err := exec.LookPath("docker")
+		if err != nil {
+			return err
+		}
+
 		cwd, err := os.Getwd()
 		if err != nil {
 			return err
 		}
 		dockerArgs := []string{
+			dockerPath,
 			"run",
 			"--interactive",
+			"--rm",
 			"--workdir", "/workdir",
 			"-v", fmt.Sprintf("%s:/workdir", cwd),
 		}
-
 		if terminal.IsTerminal(int(os.Stdin.Fd())) {
 			dockerArgs = append(dockerArgs, "--tty")
 		}
-
 		dockerArgs = append(dockerArgs, pkg.Image)
 		dockerArgs = append(dockerArgs, args[1:]...)
-		c := exec.Command("docker", dockerArgs...)
-		c.Stdin = os.Stdin
-		c.Stdout = os.Stdout
-		c.Stderr = os.Stderr
-		return c.Run()
+
+		return syscall.Exec(dockerPath, dockerArgs, os.Environ())
 	},
 }
