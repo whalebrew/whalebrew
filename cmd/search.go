12a3b3fe68cb7ecd7a4ed2d35a25a4ee65f57f1d
Ben Firshman <ben@firshman.co.uk>
2017-01-23 19:26:21 +0100

    Support for listing all packages

--- cmd/search.go
+++ cmd/search.go
@@ -6,6 +6,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
+	"sort"
 )
 
 func init() {
@@ -13,12 +14,10 @@ func init() {
 }
 
 var searchCommand = &cobra.Command{
-	Use:   "search TERM",
+	Use:   "search [TERM]",
 	Short: "Search for packages on Docker Hub",
+	Long:  "Search for Whalebrew packages on Docker Hub. If no search term is provided, all packages are listed.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return cmd.Help()
-		}
 		if len(args) > 1 {
 			return fmt.Errorf("Only one search term is supported")
 		}
@@ -28,15 +27,25 @@ var searchCommand = &cobra.Command{
 			return err
 		}
 
-		term := "whalebrew/" + args[0]
+		term := "whalebrew/"
+		if len(args) == 1 {
+			term = term + args[0]
+		}
+
 		options := types.ImageSearchOptions{Limit: 100}
 		results, err := cli.ImageSearch(context.Background(), term, options)
 		if err != nil {
 			return err
 		}
 
-		for _, res := range results {
-			fmt.Println(res.Name)
+		names := make([]string, len(results))
+		for i, result := range results {
+			names[i] = result.Name
+		}
+		sort.Strings(names)
+
+		for _, name := range names {
+			fmt.Println(name)
 		}
 
 		return nil
