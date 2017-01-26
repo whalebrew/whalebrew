1e511b584c02b17165899ca26770269272a57120
Ben Firshman <ben@firshman.co.uk>
2017-01-26 18:50:50 +0100

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/bfirsh/whalebrew/version"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Whalebrew",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Whalebrew %s\n", version.Version)
+	},
+}
---
+++ version/version.go
@@ -0,0 +1,6 @@
+package version
+
+var (
+	// Version is the current Whalebrew version
+	Version = "0.0.1"
+)
