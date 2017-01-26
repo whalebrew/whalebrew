feb3145c54d77d9db030948f82f168042d9bbd83
Ben Firshman <ben@firshman.co.uk>
2017-01-26 17:25:36 +0100

    Add tests for label configuration

--- packages/manager_test.go
+++ packages/manager_test.go
@@ -2,6 +2,7 @@ package packages
 
 import (
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"os"
@@ -53,6 +54,23 @@ func TestPackageManagerInstall(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, pkg.Name, "foo")
 	assert.Equal(t, pkg.Image, "whalebrew/foo:bar")
+
+	// test labels
+	pkg, err = NewPackageFromImageName("whalebrew/whalesay", types.ImageInspect{
+		ContainerConfig: &container.Config{
+			Labels: map[string]string{
+				"io.whalebrew.name":               "ws",
+				"io.whalebrew.config.environment": "[\"SOME_CONFIG_OPTION\"]",
+				"io.whalebrew.config.volumes":     "[\"/somesource:/somedest\"]",
+			},
+		},
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, pkg.Name, "ws")
+	assert.Equal(t, pkg.Image, "whalebrew/whalesay")
+	assert.Equal(t, pkg.Environment, []string{"SOME_CONFIG_OPTION"})
+	assert.Equal(t, pkg.Volumes, []string{"/somesource:/somedest"})
+
 }
 
 func TestPackageManagerList(t *testing.T) {
