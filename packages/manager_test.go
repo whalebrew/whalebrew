b521aef9d40d8767647cb5361b90d8f86d243a5e
Ben Firshman <ben@firshman.co.uk>
2017-01-30 00:46:08 +0100

    Refactor package into separate file

--- packages/manager_test.go
+++ packages/manager_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -49,30 +48,6 @@ func TestPackageManagerInstall(t *testing.T) {
 	err = pm.Install(pkg)
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "already exists")
-
-	// with tag
-	pkg, err = NewPackageFromImageName("whalebrew/foo:bar", types.ImageInspect{})
-	assert.Nil(t, err)
-	assert.Equal(t, pkg.Name, "foo")
-	assert.Equal(t, pkg.Image, "whalebrew/foo:bar")
-
-	// test labels
-	pkg, err = NewPackageFromImageName("whalebrew/whalesay", types.ImageInspect{
-		ContainerConfig: &container.Config{
-			Labels: map[string]string{
-				"io.whalebrew.name":               "ws",
-				"io.whalebrew.config.environment": "[\"SOME_CONFIG_OPTION\"]",
-				"io.whalebrew.config.volumes":     "[\"/somesource:/somedest\"]",
-				"io.whalebrew.config.ports":       "[\"8100:8100\"]",
-			},
-		},
-	})
-	assert.Nil(t, err)
-	assert.Equal(t, pkg.Name, "ws")
-	assert.Equal(t, pkg.Image, "whalebrew/whalesay")
-	assert.Equal(t, pkg.Environment, []string{"SOME_CONFIG_OPTION"})
-	assert.Equal(t, pkg.Volumes, []string{"/somesource:/somedest"})
-	assert.Equal(t, pkg.Ports, []string{"8100:8100"})
 
 }
 
---
+++ packages/package_test.go
@@ -0,0 +1,36 @@
+package packages
+
+import (
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPackageFromPackageName(t *testing.T) {
+	// with tag
+	pkg, err := NewPackageFromImageName("whalebrew/foo:bar", types.ImageInspect{})
+	assert.Nil(t, err)
+	assert.Equal(t, pkg.Name, "foo")
+	assert.Equal(t, pkg.Image, "whalebrew/foo:bar")
+
+	// test labels
+	pkg, err = NewPackageFromImageName("whalebrew/whalesay", types.ImageInspect{
+		ContainerConfig: &container.Config{
+			Labels: map[string]string{
+				"io.whalebrew.name":               "ws",
+				"io.whalebrew.config.environment": "[\"SOME_CONFIG_OPTION\"]",
+				"io.whalebrew.config.volumes":     "[\"/somesource:/somedest\"]",
+				"io.whalebrew.config.ports":       "[\"8100:8100\"]",
+			},
+		},
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, pkg.Name, "ws")
+	assert.Equal(t, pkg.Image, "whalebrew/whalesay")
+	assert.Equal(t, pkg.Environment, []string{"SOME_CONFIG_OPTION"})
+	assert.Equal(t, pkg.Volumes, []string{"/somesource:/somedest"})
+	assert.Equal(t, pkg.Ports, []string{"8100:8100"})
+
+}
