f827b6de97a4b89b35eeddb27b07fd12e7b1830e
Soloman Weng <soloman1124@gmail.com>
2019-02-22 18:14:47 +1000

    Ensure docker client version support

--- client/client.go
+++ client/client.go
@@ -9,7 +9,12 @@ const DefaultVersion string = "1.20"
 
 // NewClient returns a Docker client configured for Whalebrew
 func NewClient() (*client.Client, error) {
-	cli, err := client.NewEnvClient()
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.39"))
+	if err != nil {
+		return cli, err
+	}
+
+	err = client.FromEnv(cli)
 	if err != nil {
 		return cli, err
 	}
