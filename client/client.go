9116886642856e9ce2c3b9614ed7af2ba1d06798
Thibault Jamet <tjamet@users.noreply.github.com>
2019-03-04 23:23:39 +0100

    Update client creation interface.
    The latest docker client interface does not have any UpdateClientVersion any longer since https://github.com/moby/moby/commit/5f1d94e569592fc2680f8661e16d0e5b02e82492

--- client/client.go
+++ client/client.go
@@ -9,10 +9,5 @@ const DefaultVersion string = "1.20"
 
 // NewClient returns a Docker client configured for Whalebrew
 func NewClient() (*client.Client, error) {
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		return cli, err
-	}
-	cli.UpdateClientVersion(DefaultVersion)
-	return cli, nil
+	return client.NewClientWithOpts(client.WithVersion(DefaultVersion), client.FromEnv)
 }
