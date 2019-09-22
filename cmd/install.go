a3c9a36d54527d810dfec90e1ab67d3687184af1
Trevor Foster <trevor.foster@hotmail.ca>
2019-09-22 01:58:36 -0400

    ignore err val

--- cmd/install.go
+++ cmd/install.go
@@ -72,7 +72,7 @@ var installCommand = &cobra.Command{
 		pm := packages.NewPackageManager(installPath)
 
 		var installed *packages.Package
-		hasInstall, err := pm.HasInstallation(pkg.Name)
+		hasInstall, _ := pm.HasInstallation(pkg.Name)
 		if hasInstall {
 			installed, err = pm.Load(pkg.Name)
 			if !forceInstall && err != nil {
