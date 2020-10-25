a1c3d31cf40cb5fd9f8b6c637abf3c0e0812d7f1
Thibault Jamet <tjamet@users.noreply.github.com>
2020-10-25 16:58:13 +0100

    Add support for multiple search repositories (#108)
    * Add support for multiple search repositories
    
    Up until now, whalebrew restricts its searches to https://hub.docker.com/u/whalebrew
    
    This commit introduces the ability to customize search organization
    under docker hub.
    
    To customize it, place a file like
    
    ```
    registries:
    - dockerHub:
        owner: whalebrew
    ```
    
    You can add as many search repositories as desired, all of them will be
    searched.
    
    Currently, there is no authentication to the search repositories. Only
    public images will be retrieved.
    
    A step forward for whalebrew tap discussed in #102

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
+)
+
+type DockerHubRegistry struct {
+	Owner string `yaml:"owner"`
+}
+
+type Registry struct {
+	DockerHub *DockerHubRegistry `yaml:"dockerHub"`
+}
+
+type Config struct {
+	Registries []Registry `yaml:"registries"`
+}
+
+func GetConfig() Config {
+	configPath := filepath.Join(viper.GetString("config_dir"), "config.yaml")
+	fd, err := os.Open(configPath)
+	c := Config{}
+	if err == nil {
+		defer fd.Close()
+		yaml.NewDecoder(fd).Decode(&c)
+	}
+	if len(c.Registries) == 0 {
+		c.Registries = []Registry{{DockerHub: &DockerHubRegistry{Owner: "whalebrew"}}}
+	}
+	return c
+}
