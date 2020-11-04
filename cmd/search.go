ede641e87dfdf958215bcde3b3686d9163082d7e
Thibault Jamet <tjamet@users.noreply.github.com>
2020-11-04 19:51:24 +0100

    Feat: add support to search on any docker registry (#115)
    Refactor how search is actually working to allow hooking and injecting
    several search methods.
    The alternative search method is using the registry
    [catalog](https://docs.docker.com/registry/spec/api/#catalog) listing
    all images in the registry and filtering them according to the searched
    term.
    
    Though this method is particularly inefficient with large registries.
    We should at some point implement some kind of cache to speed up
    consequent searches and avoid over-loading the remote server.
    Similarly, to improve performances, we should probably think of a way to
    integrate directly with the registries APIs so we can get better
    performances.
    
    Finally, this method can not be used on docker hub, that still uses the
    dedicated docker hub search method, as the catalog has been
    [disabled on docker hub](https://stackoverflow.com/a/37518393)
    because it would list all the images available on docker hub
    
    fixes: #102

--- cmd/search.go
+++ cmd/search.go
@@ -1,23 +1,13 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/whalebrew/whalebrew/config"
+	"github.com/whalebrew/whalebrew/search"
 )
-
-type imageResult struct {
-	User string `json:"user"`
-	Name string `json:"name"`
-}
-
-type searchAnswer struct {
-	Results []imageResult `json:"results"`
-}
 
 func init() {
 	RootCmd.AddCommand(searchCommand)
@@ -31,36 +21,14 @@ var searchCommand = &cobra.Command{
 		if len(args) > 1 {
 			return fmt.Errorf("Only one search term is supported")
 		}
-		for _, registry := range config.GetConfig().Registries {
-			if registry.DockerHub != nil {
-				params := url.Values{}
-				params.Set("page_size", "100")
-				params.Set("ordering", "last_updated")
-				if len(args) > 0 {
-					params.Set("name", args[0])
-				}
-				u := url.URL{
-					Scheme:   "https",
-					Host:     "hub.docker.com",
-					Path:     fmt.Sprintf("/v2/repositories/%s/", registry.DockerHub.Owner),
-					RawQuery: params.Encode(),
-				}
-
-				r, err := http.Get(u.String())
-				if err != nil {
-					return err
-				}
-				answer := searchAnswer{}
-				err = json.NewDecoder(r.Body).Decode(&answer)
-				if err != nil {
-					return err
-				}
-				for _, image := range answer.Results {
-					fmt.Printf("%s/%s\n", image.User, image.Name)
-				}
-			} else {
-				err := fmt.Errorf("Unsupported registry %v. Only docker revistry is supported", registry)
-				return err
+		errHandler := func(err error) bool {
+			fmt.Println(err.Error())
+			os.Exit(1)
+			return true
+		}
+		for searcher := range search.ForRegistries(config.GetConfig().Registries, errHandler) {
+			for image := range searcher.Search(args[0], errHandler) {
+				fmt.Println(image)
 			}
 		}
 		return nil
