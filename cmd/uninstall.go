35c66ec4bded626a9804781d48112607ccfd065e
Thibault Jamet <tjamet@users.noreply.github.com>
2022-03-05 17:22:42 +0100

    Detect package or image name in uninstall
    While the install commands accepts an image name, the uninstall command
    expects the package name.
    This is confusing for users as the command is not symetrical.
    
    Implement a simple detection mechanism that allows to detect the
    relevant package to be deleted so the uninstall command can be used
    using the image name as the install endpoint accepts. The previous
    behaviour to be able to uninstall a package from its name is kept
    
    fixes #162
    
    Change-Id: Ibdddfdb9da2680e6c0c2866dc15e27d331c20f47

--- cmd/uninstall.go
+++ cmd/uninstall.go
@@ -2,9 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"path"
 
-        "github.com/Songmu/prompter"
+	"github.com/Songmu/prompter"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/whalebrew/whalebrew/hooks"
@@ -19,8 +20,20 @@ func init() {
 	RootCmd.AddCommand(uninstallCommand)
 }
 
+type deleteReason string
+
+var (
+	deleteReasonPackageNameMatches  deleteReason = "package name matches"
+	deleteReasonPackageImageMatches deleteReason = "package image matches"
+)
+
+type deletionCandidate struct {
+	pkg    *packages.Package
+	reason deleteReason
+}
+
 var uninstallCommand = &cobra.Command{
-	Use:   "uninstall PACKAGENAME",
+	Use:   "uninstall PACKAGENAME|IMAGENAME",
 	Short: "Uninstall a package",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
@@ -31,26 +44,55 @@ var uninstallCommand = &cobra.Command{
 		}
 
 		pm := packages.NewPackageManager(viper.GetString("install_path"))
-		packageName := args[0]
+		packageNameOrImage := args[0]
 
-		path := path.Join(pm.InstallPath, packageName)
-
-		if err := hooks.Run("pre-uninstall", packageName); err != nil {
+		if err := hooks.Run("pre-uninstall", packageNameOrImage); err != nil {
 			return fmt.Errorf("pre-uninstall install script failed: %s", err.Error())
 		}
-		
-                if !assumeYes {
-                	if !prompter.YN(fmt.Sprintf("This will permanently delete '%s'. Are you sure?", path), false) {
+		packages, err := pm.List()
+		if err != nil {
+			return fmt.Errorf("unable to list packages: %v", err)
+		}
+
+		candidates := []deletionCandidate{}
+		for _, pkg := range packages {
+			if pkg.Name == packageNameOrImage {
+				candidates = append(candidates, deletionCandidate{
+					pkg:    pkg,
+					reason: deleteReasonPackageNameMatches,
+				})
+			} else if pkg.Image == packageNameOrImage {
+				candidates = append(candidates, deletionCandidate{
+					pkg:    pkg,
+					reason: deleteReasonPackageImageMatches,
+				})
+			}
+		}
+		if len(candidates) > 1 {
+			fmt.Fprintln(os.Stderr, "Many mathcing packages found. Run:")
+			for _, candidate := range candidates {
+				fmt.Fprintln(os.Stderr, "'whalebrew uninstall ", candidate.pkg.Name, "' to uninstall image ", candidate.pkg.Image)
+			}
+			os.Exit(1)
+		}
+		if len(candidates) == 0 {
+			fmt.Fprintf(os.Stderr, "Unable to find a package with name or image %s\n", packageNameOrImage)
+			return nil
+		}
+
+		path := path.Join(pm.InstallPath, candidates[0].pkg.Name)
+		if !assumeYes {
+			if !prompter.YN(fmt.Sprintf("This will permanently delete '%s'. Are you sure?", path), false) {
 				return nil
 			}
 		}
 
-		err := pm.Uninstall(packageName)
+		err = pm.Uninstall(packageNameOrImage)
 		if err != nil {
 			return err
 		}
 
-		if err := hooks.Run("post-uninstall", packageName); err != nil {
+		if err := hooks.Run("post-uninstall", packageNameOrImage); err != nil {
 			return fmt.Errorf("post-uninstall install script failed: %s", err.Error())
 		}
 		fmt.Printf("🚽  Uninstalled %s\n", path)
